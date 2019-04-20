06c7fa42cad1726d3956d1d90556a783f90878b7
wesleyflorence <jw37g@mac.com>
2019-04-19 23:12:59 -0700

    perfecting front end login flow

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  @ViewChild('f') loginForm: NgForm;
+  username: string;
+  password: string;
+  errorFlag: boolean;
+  errorMessage = 'Invalid username or password';
 
-  constructor() { }
+  constructor(private router: Router) { 
+    this.errorFlag = false;
+  }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.username = this.loginForm.value.username;
+    this.password = this.loginForm.value.password;
+    console.log(this.username, this.password);
+
   }
 
 }
--- src/app/components/user/register/register.component.ts
+++ src/app/components/user/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  @ViewChild('f') loginForm: NgForm;
+  username: string;
+  password: string;
+  verify: string;
+  errorFlag: boolean;
+  errorMessage = 'Invalid username or password'
 
-  constructor() { }
+  constructor(private router: Router) { 
+    this.errorFlag = false;
+  }
 
   ngOnInit() {
+  }
+
+  register() {
+    this.username = this.loginForm.value.username;
+    this.password = this.loginForm.value.password;
+    console.log(this.username, this.password);
+
+    if (this.password !== this.verify) {
+      this.errorMessage = 'The two passwords provided do not match'
+      this.errorFlag = true;
+    }
   }
 
 }
